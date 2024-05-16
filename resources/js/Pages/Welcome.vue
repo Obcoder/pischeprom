88f05ea5d6023cd22597a21a2b85a7ae166112a9
Obcoder <obcoder@gmail.com>
2024-05-16 13:42:24 +0300

    Start developing :wq
    Start developing
    :wq

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -28,6 +28,15 @@ function handleImageError() {
 
 <template>
     <Head title="Welcome" />
+
+    <h1>
+        УРРРРРААААААА!!!!!! Работает!!!!!!!!!!
+    </h1>
+
+    <p>
+        Start developing
+    </p>
+
     <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
         <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" />
         <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
