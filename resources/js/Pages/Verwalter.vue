f073006ecb5d3deb55644343f91b6686db5012f8
Obcoder <obcoder@gmail.com>
2024-11-17 00:44:48 +0300

    Verwalter.Fix 35: Unit.show

--- resources/js/Pages/Verwalter.vue
+++ resources/js/Pages/Verwalter.vue
@@ -302,7 +302,7 @@ function storeProduct(){
                                               hover="hover"
                                 >
                                     <template v-slot:item.name="{ item }">
-                                        <Link :href="route('unit.show')" :data="{id: item.id}">
+                                        <Link :href="route('unit.show')">
                                             {{item.name}}
                                         </Link>
                                     </template>
