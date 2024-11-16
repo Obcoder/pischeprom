cf55c7c05d3c14f05ceb0ce3295227e49a7660e0
Obcoder <obcoder@gmail.com>
2024-11-16 22:54:58 +0300

    Verwalter.Fix 21: Unit.show

---
+++ app/Http/Controllers/UnitController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Unit;
+use Inertia\Inertia;
+
+class UnitController extends Controller
+{
+    public function show(Unit $unit){
+        $data = ['unit' => $unit];
+        return Inertia::render('Unit', $data);
+    }
+}
