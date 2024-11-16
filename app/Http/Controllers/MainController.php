887eedf8eb03a158bc1be4dfd1ef30e30df992d7
Obcoder <obcoder@gmail.com>
2024-11-16 17:33:58 +0300

    Good of the day script added

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Good;
 use App\Models\GoodOfTheDay;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -12,8 +13,11 @@ class MainController extends Controller
     public function index()
     {
         $categories = Category::all();
-        $goodOfTheDay = GoodOfTheDay::with('good')
-            ->inRandomOrder()->first();
+        $good = Good::inRandomOrder()->first();
+        $goodOfTheDay = GoodOfTheDay::create([
+            'good_id' => $good['id'],
+            'date' => date('Y-m-d'),
+                                             ]);
 
         $data = [
             'categories' => $categories,
