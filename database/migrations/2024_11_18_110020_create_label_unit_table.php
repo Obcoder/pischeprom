73023057f6df03a78770f5694d64e84ec8022be0
Obcoder <obcoder@gmail.com>
2024-11-18 14:02:44 +0300

    Labels initial

---
+++ database/migrations/2024_11_18_110020_create_label_unit_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('label_unit', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->bigInteger('label_id')->unsigned()->index();
+            $table->foreign('label_id')->references('id')->on('labels')->onDelete('cascade');
+            $table->bigInteger('unit_id')->unsigned()->index();
+            $table->foreign('unit_id')->references('id')->on('units')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('label_unit');
+    }
+};
