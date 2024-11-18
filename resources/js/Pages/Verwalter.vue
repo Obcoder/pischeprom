289b084deb2bd90a2527f63e2eb69c7b931e1c2d
Obcoder <obcoder@gmail.com>
2024-11-18 20:17:06 +0300

    headersCountries fix 8

--- resources/js/Pages/Verwalter.vue
+++ resources/js/Pages/Verwalter.vue
@@ -42,8 +42,8 @@ onMounted(()=>{
             key: 'name',
         },
         {
-            title: 'codeTelefon',
-            value: 'codeTelefon',
+            title: 'сodeTelefon',
+            value: 'сodeTelefon',
         },
         {
             title: 'codeISO',
