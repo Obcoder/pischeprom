daed934b9f41627f07ad85b95a926ad338ef2ba6
Obcoder <obcoder@gmail.com>
2024-11-21 14:35:05 +0300

    Stages added

---
+++ database/migrations/2024_11_21_113312_create_stage_unit_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stage_unit', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->unsignedBigInteger('stage_id');
+            $table->unsignedBigInteger('unit_id');
+            $table->foreign('stage_id')->references('id')->on('stages')->onDelete('cascade');
+            $table->foreign('unit_id')->references('id')->on('units')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stage_unit');
+    }
+};
