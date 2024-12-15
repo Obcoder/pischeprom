80518ff8e6301b461d250a861ce494e9857cdfe0
Obcoder <obcoder@gmail.com>
2024-12-15 20:31:51 +0300

    Verwalter: Check Form Fix-12

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -7,10 +7,10 @@ use Illuminate\Http\Request;
 
 class CheckController extends Controller
 {
-    public function show($id){
+    public function show(string $id){
         $check = Check::with('entity')
             ->find($id);
 
-        return Inertia::render('Checks', $check->id);
+        return Inertia::render('Checks', $check);
     }
 }
