c039f42896080d495512f90c2bad84f1a8559ebe
Obcoder <obcoder@gmail.com>
2024-12-24 20:50:16 +0300

    Config: mail + mailgun

--- config/mail.php
+++ config/mail.php
@@ -90,6 +90,13 @@ return [
             ],
         ],
 
+        'mailgun' => [
+            'transport' => 'mailgun',
+            'domain' => env('MAILGUN_DOMAIN'),
+            'secret' => env('MAILGUN_SECRET'),
+            'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        ],
+
     ],
 
     /*
