14eeeca3ddb66ee125cc99dd17b7bb46cfa79ddc
Obcoder <obcoder@gmail.com>
2024-12-31 00:23:34 +0300

    YandexRequests table added

---
+++ database/migrations/2024_12_30_212132_create_yandex_requests_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('yandex_requests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->string('request');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('yandex_requests');
+    }
+};
