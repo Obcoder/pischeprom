440e72da27029f1655f009a84933b7f7e9da1796
Obcoder <obcoder@gmail.com>
2025-02-13 18:06:21 +0300

    Verwalter: Units data table dev-20250213-08

--- resources/js/Pages/Verwalter.vue
+++ resources/js/Pages/Verwalter.vue
@@ -585,7 +585,7 @@ async function sendMail() {
                                     <template v-slot:item.products="{item}">
                                         <span v-for="product in item.products">
                                             {{product.rus}}
-                                            {{product.pivot.action}}
+                                            {{product.action}}
                                         </span>
                                     </template>
                                 </v-data-table>
