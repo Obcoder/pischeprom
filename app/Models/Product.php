53f5dd126716d9ad50c362d43c8253d912d9898f
Obcoder <obcoder@gmail.com>
2025-02-13 21:47:42 +0300

    Verwalter: Units data table dev-20250213-25

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -26,6 +26,6 @@ class Product extends Model
 
     public function action()
     {
-        return $this->hasOneThrough(Action::class, product_unit::class, 'id', 'id', 'product_id', 'action_id');
+        return $this->hasOneThrough(Action::class, product_unit::class, 'product_id', 'action_id', 'id', 'id');
     }
 }
