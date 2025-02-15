6533daa0ba620e1371b4aa8ac8d5b820fe716c72
Obcoder <obcoder@gmail.com>
2025-02-15 11:53:48 +0300

    Verwalter: Units data table dev-20250213-53

--- app/Models/product_unit.php
+++ app/Models/product_unit.php
@@ -9,7 +9,7 @@ class product_unit extends Pivot
     protected $table = 'product_unit';
     public function action()
     {
-        return $this->belongsTo(Action::class, 'action_id')
+        return $this->hasOne(Action::class, 'action_id')
             ->withDefault();
     }
 }
