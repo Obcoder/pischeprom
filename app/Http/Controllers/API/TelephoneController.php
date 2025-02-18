358e602db8730b8563aac008a27145f1a0c0c36b
Obcoder <obcoder@gmail.com>
2025-02-18 22:53:51 +0300

    Telephone model api created

---
+++ app/Http/Controllers/API/TelephoneController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Telephone;
+use Illuminate\Http\Request;
+
+class TelephoneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $like = $request->search;
+        $telephones = Telephone::where('number', 'like', "%{$like}%")
+            ->orderBy('number', 'desc')
+            ->get();
+        return $telephones;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
