e8e31106eb69ffd9f74c419415d090d79f4715fe
Obcoder <obcoder@gmail.com>
2025-02-25 20:05:06 +0300

    MyTestMail: dev-20250225-12

--- app/Mail/MyTestMail.php
+++ app/Mail/MyTestMail.php
@@ -47,7 +47,7 @@ class MyTestMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.myFirstEmail',
+            view: 'emails.funEmail',
         );
     }
 
