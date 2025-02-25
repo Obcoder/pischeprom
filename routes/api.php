2f2f0b46a50ed1fa897cb2fdc7a061111a50266a
Obcoder <obcoder@gmail.com>
2025-02-25 15:48:12 +0300

    MyTestMail: dev-20250225-04

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/api/mail', [\App\Http\Controllers\MailController::class, 'sendMail'])
+Route::post('/mail', [\App\Http\Controllers\MailController::class, 'sendMail'])
     ->name('api.mail');
