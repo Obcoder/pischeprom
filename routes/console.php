2e68230e7dcd0814b37098fe9764bb3052793536
Obcoder <obcoder@gmail.com>
2025-03-02 15:36:50 +0300

    Changes about sitemap made

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Spatie\Sitemap\SitemapGenerator;
+
+Artisan::command('sitemap:generate', function () {
+    SitemapGenerator::create('https://пищепром-сервер.рф')
+        ->writeToFile(public_path('sitemap.xml'));
+
+    $this->info('Sitemap generated successfully!');
+});
+
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
