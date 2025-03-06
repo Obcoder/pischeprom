4ef09c1eb5949fe986071d5b6b3a645f135001f9
Obcoder <obcoder@gmail.com>
2025-03-06 15:46:55 +0300

    Products: Form Consumption dev-20250306-15

--- routes/web.php
+++ routes/web.php
@@ -107,7 +107,7 @@ Route::post('/api/good/store', [\App\Http\Controllers\API\GoodController::class,
 Route::post('/api/checkcommodity/store', [\App\Http\Controllers\API\CheckCommodityController::class, 'store'])
     ->name('api.checkcommodity.store');
 Route::post('/api/consumption/store', [\App\Http\Controllers\API\ConsumptionController::class, 'store'])
-    ->name( 'api.product.store');
+    ->name( 'api.consumption.store');
 
 /*
 |--------------------------------------------------------------------------
