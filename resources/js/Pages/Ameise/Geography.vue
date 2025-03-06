66d4e1610b3570c5eed260f631a519a6d23ff366
Obcoder <obcoder@gmail.com>
2025-03-06 17:37:48 +0300

    Geography dev-20250306-02

--- resources/js/Pages/Ameise/Geography.vue
+++ resources/js/Pages/Ameise/Geography.vue
@@ -35,19 +35,59 @@ onMounted(()=> {
 <template>
     <v-container>
         <v-row>
-            <v-col>
+            <v-col cols="1">
                 <v-card>
                     <v-card-title>Countries</v-card-title>
                     <v-card-text>
-                        <v-data-table :items="countries"
-                                      density="compact"
-                                      hover="true"
-                        ></v-data-table>
+                        <v-list>
+                            <v-list-item v-for="country in countries"
+                                         class="text-[12px] font-sans"
+                            >
+                                {{country.name}}
+                            </v-list-item>
+                        </v-list>
                     </v-card-text>
                 </v-card>
             </v-col>
-            <v-col>
-                {{regions}}
+            <v-col cols="3">
+                <v-card>
+                    <v-card-title>Cities</v-card-title>
+                    <v-card-text>
+                        <v-list>
+                            <v-list-item v-for="city in cities">
+                                {{city.name}}
+                            </v-list-item>
+                        </v-list>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="1">
+                <v-card>
+                    <v-card-title>Regions</v-card-title>
+                    <v-card-text>
+                        <v-list>
+                            <v-list-item v-for="region in regions"
+                                         class="text-[12px] font-sans"
+                            >
+                                {{region.name}}
+                            </v-list-item>
+                        </v-list>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+            <v-col cols="5">
+                <v-card>
+                    <v-card-text>Buildings</v-card-text>
+                    <v-card-text>
+                        <v-list>
+                            <v-list-item v-for="building in buildings">
+                                {{building.address}}
+                            </v-list-item>
+                        </v-list>
+                    </v-card-text>
+                </v-card>
             </v-col>
         </v-row>
     </v-container>
