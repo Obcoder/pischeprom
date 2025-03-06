bf6ed5555f4a73ed1706de75c2fca57160d98f05
Obcoder <obcoder@gmail.com>
2025-03-06 18:46:43 +0300

    Geography dev-20250306-07

--- resources/js/Pages/Ameise/Geography.vue
+++ resources/js/Pages/Ameise/Geography.vue
@@ -37,80 +37,82 @@ onMounted(()=> {
 <template>
     <v-container fluid>
         <v-row>
-            <v-card>
-                <v-tabs v-model="tab"
-                        align-tabs="center"
-                        color="deep-purple-accent-4"
-                >
-                    <v-tab value="countries">Countries</v-tab>
-                    <v-tab value="cities">Cities</v-tab>
-                </v-tabs>
-                <v-card-text>
-                    <v-tabs-window v-model="tab">
-                        <v-tabs-window-item value="countries">
-                            <v-row>
-                                <v-col>
-                                    <v-card>
-                                        <v-card-title>Countries</v-card-title>
-                                        <v-card-text>
-                                            <v-list>
-                                                <v-list-item v-for="country in countries"
-                                                             class="text-[12px] font-sans"
-                                                >
-                                                    {{country.name}}
-                                                </v-list-item>
-                                            </v-list>
-                                        </v-card-text>
-                                    </v-card>
-                                </v-col>
-                                <v-col>
-                                    <v-card>
-                                        <v-card-title>Regions</v-card-title>
-                                        <v-card-text>
-                                            <v-list>
-                                                <v-list-item v-for="region in regions"
-                                                             class="text-[12px] font-sans"
-                                                >
-                                                    {{region.name}}
-                                                </v-list-item>
-                                            </v-list>
-                                        </v-card-text>
-                                    </v-card>
-                                </v-col>
-                            </v-row>
-                        </v-tabs-window-item>
-                        <v-tabs-window-item value="cities"
-                        >
-                            <v-row>
-                                <v-col>
-                                    <v-card>
-                                        <v-card-title>Cities</v-card-title>
-                                        <v-card-text>
-                                            <v-list>
-                                                <v-list-item v-for="city in cities">
-                                                    {{city.name}}
-                                                </v-list-item>
-                                            </v-list>
-                                        </v-card-text>
-                                    </v-card>
-                                </v-col>
-                                <v-col>
-                                    <v-card>
-                                        <v-card-text>Buildings</v-card-text>
-                                        <v-card-text>
-                                            <v-list>
-                                                <v-list-item v-for="building in buildings">
-                                                    {{building.address}}
-                                                </v-list-item>
-                                            </v-list>
-                                        </v-card-text>
-                                    </v-card>
-                                </v-col>
-                            </v-row>
-                        </v-tabs-window-item>
-                    </v-tabs-window>
-                </v-card-text>
-            </v-card>
+            <v-col>
+                <v-card>
+                    <v-tabs v-model="tab"
+                            align-tabs="center"
+                            color="deep-purple-accent-4"
+                    >
+                        <v-tab value="countries">Countries</v-tab>
+                        <v-tab value="cities">Cities</v-tab>
+                    </v-tabs>
+                    <v-card-text>
+                        <v-tabs-window v-model="tab">
+                            <v-tabs-window-item value="countries">
+                                <v-row>
+                                    <v-col>
+                                        <v-card>
+                                            <v-card-title>Countries</v-card-title>
+                                            <v-card-text>
+                                                <v-list>
+                                                    <v-list-item v-for="country in countries"
+                                                                 class="text-[12px] font-sans"
+                                                    >
+                                                        {{country.name}}
+                                                    </v-list-item>
+                                                </v-list>
+                                            </v-card-text>
+                                        </v-card>
+                                    </v-col>
+                                    <v-col>
+                                        <v-card>
+                                            <v-card-title>Regions</v-card-title>
+                                            <v-card-text>
+                                                <v-list>
+                                                    <v-list-item v-for="region in regions"
+                                                                 class="text-[12px] font-sans"
+                                                    >
+                                                        {{region.name}}
+                                                    </v-list-item>
+                                                </v-list>
+                                            </v-card-text>
+                                        </v-card>
+                                    </v-col>
+                                </v-row>
+                            </v-tabs-window-item>
+                            <v-tabs-window-item value="cities"
+                            >
+                                <v-row>
+                                    <v-col>
+                                        <v-card>
+                                            <v-card-title>Cities</v-card-title>
+                                            <v-card-text>
+                                                <v-list>
+                                                    <v-list-item v-for="city in cities">
+                                                        {{city.name}}
+                                                    </v-list-item>
+                                                </v-list>
+                                            </v-card-text>
+                                        </v-card>
+                                    </v-col>
+                                    <v-col>
+                                        <v-card>
+                                            <v-card-text>Buildings</v-card-text>
+                                            <v-card-text>
+                                                <v-list>
+                                                    <v-list-item v-for="building in buildings">
+                                                        {{building.address}}
+                                                    </v-list-item>
+                                                </v-list>
+                                            </v-card-text>
+                                        </v-card>
+                                    </v-col>
+                                </v-row>
+                            </v-tabs-window-item>
+                        </v-tabs-window>
+                    </v-card-text>
+                </v-card>
+            </v-col>
         </v-row>
     </v-container>
 </template>
