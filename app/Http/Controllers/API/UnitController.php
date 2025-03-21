ae3775b39f48acc8ba5b6cfbe050091e18450bc0
Obcoder <obcoder@gmail.com>
2025-03-21 21:53:22 +0300

    Unit: files-dev-07

--- app/Http/Controllers/API/UnitController.php
+++ app/Http/Controllers/API/UnitController.php
@@ -93,9 +93,9 @@ class UnitController extends Controller
         //
     }
 
-    public function getUnitFiles($unitName): array
+    public function getUnitFiles($unitId): array
     {
-        $path = "units/{$unitName}/"; // Путь внутри storage/app/units/{unit_name}
+        $path = "/units/{$unitId}/"; // Путь внутри storage/app/units/{unit_name}
 
         if (!Storage::exists($path)) {
             return []; // Если папки нет, вернуть пустой список
