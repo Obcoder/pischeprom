ded72bb24aa4f388b4c3e9d14c074fc4e1f73ae1
Obcoder <obcoder@gmail.com>
2025-03-21 21:30:14 +0300

    Unit: files-dev-03

--- resources/js/Pages/Unit.vue
+++ resources/js/Pages/Unit.vue
@@ -182,11 +182,7 @@ const headersEntities = ref([
 
 let files = ref();
 function fetchFiles(name){
-    axios.get(route('api.unit.getFiles'), {
-        params: {
-            unitName: name,
-        }
-    }).then(function (response){
+    axios.get(route('api.unit.getFiles'), { unitName: name }).then(function (response){
         files.value = response.data;
     }).catch (function  (error) {
         console.error('Ошибка загрузки файлов:', error);
