cfaa2148bcd9b9af9d2da0dfce415dd2538298c5
Obcoder <obcoder@gmail.com>
2025-03-27 22:09:03 +0300

    Cities: dev-20250327-05

--- resources/js/Pages/Ameise/Cities.vue
+++ resources/js/Pages/Ameise/Cities.vue
@@ -57,7 +57,7 @@ function storeCity(){
         preserveScroll: true,
         onSuccess: ()=> {
             formCity.reset();
-            indexCities(searchCitiesLike)
+            indexCities(searchCitiesLike.value)
         },
     })
 }
