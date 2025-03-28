4d384c9ca508885c461cb8f331e13676a2e090f8
Obcoder <obcoder@gmail.com>
2025-03-28 23:05:06 +0300

    Goods: dev-20250328-02

--- resources/js/Pages/Ameise/Goods.vue
+++ resources/js/Pages/Ameise/Goods.vue
@@ -113,32 +113,34 @@ onMounted(()=>{
             <v-col></v-col>
         </v-row>
         <v-row>
-            <v-col class="flex flex-row">
-                <v-card v-for="good in goods"
-                        color="#952175">
-                    <div class="d-flex flex-no-wrap justify-space-between">
-                        <div>
-                            <v-card-title class="text-h5">
-                                {{good.name}}
-                            </v-card-title>
-                            <v-card-actions>
-                                <v-btn
-                                    class="ms-2"
-                                    icon="mdi-play"
-                                    variant="text"
-                                ></v-btn>
-                            </v-card-actions>
-                        </div>
+            <v-col>
+                <v-col class="flex flex-row">
+                    <v-card v-for="good in goods"
+                            color="#952175">
+                        <div class="d-flex">
+                            <div>
+                                <v-card-title class="text-h6">
+                                    {{good.name}}
+                                </v-card-title>
+                                <v-card-actions>
+                                    <v-btn
+                                        class="ms-2"
+                                        icon="mdi-play"
+                                        variant="text"
+                                    ></v-btn>
+                                </v-card-actions>
+                            </div>
 
-                        <v-avatar
-                            class="ma-3"
-                            rounded="0"
-                            size="125"
-                        >
-                            <v-img src={{good.ava_image}}></v-img>
-                        </v-avatar>
-                    </div>
-                </v-card>
+                            <v-avatar
+                                class="ma-3"
+                                rounded="0"
+                                size="125"
+                            >
+                                <v-img :src="good.ava_image || logo"></v-img>
+                            </v-avatar>
+                        </div>
+                    </v-card>
+                </v-col>
             </v-col>
         </v-row>
     </v-container>
