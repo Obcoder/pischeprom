44835937bf9a6088d8d40181ab5fb27d29bd4a0f
Obcoder <obcoder@gmail.com>
2025-03-29 16:26:14 +0300

    Site.Goods: dev-20250329-12

--- resources/js/Pages/Goods.vue
+++ resources/js/Pages/Goods.vue
@@ -13,39 +13,33 @@
             <v-col></v-col>
             <v-col></v-col>
         </v-row>
-        <v-row class="h-100">
-            <v-col v-for="good in goods"
-                   :key="good.id"
-                   cols="2"
-            >
-                <v-card color="white"
-                        class="mx-2"
+        <v-row>
+            <v-col>
+                <v-card v-for="good in goods"
+                        :key="good.id"
+                        class="w-50 mr-2"
                         rounded
                 >
                     <v-img :src="good.ava_image || logo"></v-img>
-                    <div class="d-flex">
-                        <div>
-                            <v-card-subtitle class="font-sans text-wrap">
-                                {{good.name}}
-                            </v-card-subtitle>
-                            <v-card-actions>
-                                <v-btn text="заказать"
-                                       density="comfortable"
-                                       class="ms-2"
-                                       variant="tonal"
-                                ></v-btn>
-                                <v-divider opacity="80"
-                                           color="grey"
-                                           vertical
-                                ></v-divider>
-                                <v-btn text="подробнее"
-                                       density="comfortable"
-                                       class="ms-2"
-                                       variant="flat"
-                                ></v-btn>
-                            </v-card-actions>
-                        </div>
-                    </div>
+                    <v-card-subtitle class="font-sans text-wrap">
+                        {{good.name}}
+                    </v-card-subtitle>
+                    <v-card-actions>
+                        <v-btn text="заказать"
+                               density="comfortable"
+                               class="ms-2"
+                               variant="tonal"
+                        ></v-btn>
+                        <v-divider opacity="80"
+                                   color="grey"
+                                   vertical
+                        ></v-divider>
+                        <v-btn text="подробнее"
+                               density="comfortable"
+                               class="ms-2"
+                               variant="flat"
+                        ></v-btn>
+                    </v-card-actions>
                 </v-card>
             </v-col>
         </v-row>
