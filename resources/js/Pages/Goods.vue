4aedc8edbd1d558ef0cf3d4406cc2807772c49fd
Obcoder <obcoder@gmail.com>
2025-03-29 13:51:48 +0300

    Site.Goods: dev-20250329-01

--- resources/js/Pages/Goods.vue
+++ resources/js/Pages/Goods.vue
@@ -13,45 +13,52 @@
             <v-col></v-col>
             <v-col></v-col>
         </v-row>
-        <v-row class="d-flex justify-start">
-            <v-card v-for="good in goods"
-                    color="white"
-                    class="mx-3 border border-1 border-pink-950 border-opacity-75"
-                    rounded
+        <v-row>
+            <v-col v-for="good in goods"
+                   :key="good.id"
+                   cols="12"
+                   sm="6"
+                   md="4"
+                   lg="3"
             >
-                <div class="d-flex">
-                    <div>
-                        <v-card-title class="text-xs font-sans bg-teal-200">
-                            {{good.name}}
-                        </v-card-title>
-                        <v-card-actions>
-                            <v-btn text="заказать"
-                                   density="comfortable"
-                                   class="ms-2"
-                                   variant="tonal"
-                            ></v-btn>
-                            <v-divider opacity="80"
-                                       color="grey"
-                                       vertical
-                            ></v-divider>
-                            <v-btn text="подробнее"
-                                   density="comfortable"
-                                   class="ms-2"
-                                   variant="flat"
-                            ></v-btn>
-                        </v-card-actions>
-                    </div>
-
-                    <v-avatar
-                        class="ga-1"
-                        size="112"
+                <v-card color="white"
+                        class="mx-3 border border-1 border-pink-950 border-opacity-75"
                         rounded
-                        border
-                    >
-                        <v-img :src="good.ava_image || logo"></v-img>
-                    </v-avatar>
-                </div>
-            </v-card>
+                >
+                    <div class="d-flex">
+                        <div>
+                            <v-card-title class="text-xs font-sans bg-teal-200">
+                                {{good.name}}
+                            </v-card-title>
+                            <v-card-actions>
+                                <v-btn text="заказать"
+                                       density="comfortable"
+                                       class="ms-2"
+                                       variant="tonal"
+                                ></v-btn>
+                                <v-divider opacity="80"
+                                           color="grey"
+                                           vertical
+                                ></v-divider>
+                                <v-btn text="подробнее"
+                                       density="comfortable"
+                                       class="ms-2"
+                                       variant="flat"
+                                ></v-btn>
+                            </v-card-actions>
+                        </div>
+
+                        <v-avatar
+                            class="ga-1"
+                            size="112"
+                            rounded
+                            border
+                        >
+                            <v-img :src="good.ava_image || logo"></v-img>
+                        </v-avatar>
+                    </div>
+                </v-card>
+            </v-col>
         </v-row>
     </v-container>
 </template>
