0c7eafcf923a32c5dba83a1431ba1287d711aaeb
Obcoder <obcoder@gmail.com>
2025-04-08 17:55:47 +0300

    Welcome: dev-20250408-04 fix-01

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -47,13 +47,15 @@ useHead({
 <template>
     <v-container fluid>
         <v-row>
-            <v-col class="flex flex-row">
+            <v-col>
                 <v-img src="https://storage.yandexcloud.net/pps/goods/38/avatar-66546.webp"
                        :width="267"
                        aspect-ratio="1/1"
                        cover
                        rounded
                 ></v-img>
+            </v-col>
+            <v-col>
                 <v-img src="https://storage.yandexcloud.net/pps/goods/26/1024%D1%85628_5.jpg"
                        :width="267"
                        aspect-ratio="1/1"
