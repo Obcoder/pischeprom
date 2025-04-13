e4149f08f0d6fa3a897bf5d073890c4ffc4d855e
Obcoder <obcoder@gmail.com>
2025-04-13 23:55:13 +0300

    Verwalter.Unit: dev-20250413-09-sendEmail+selectedProducts fix-09

--- resources/views/emails/funEmail.blade.php
+++ resources/views/emails/funEmail.blade.php
@@ -117,7 +117,7 @@
                 <th>Товар</th>
                 <th>Цена</th>
             </tr>
-            @foreach($products as $product)
+            @foreach($details['products'] as $product)
                 <tr class="data">
                     <td><strong>{{ $product }}</strong></td>
                     <td style="vertical-align: top;">цена</td>
