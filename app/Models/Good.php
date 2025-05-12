57e494dcbf70768b0e7045ef927ab1cadf564d54
Obcoder <obcoder@gmail.com>
2025-05-12 16:03:58 +0300

    Verwalter.Good: dev-20250512-01-prices-fix-01

--- app/Models/Good.php
+++ app/Models/Good.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
@@ -32,8 +33,8 @@ class Good extends Model
     {
         return $this->belongsToMany(Product::class);
     }
-    public function prices(): HasMany
+    public function prices(): BelongsTo
     {
-        return $this->hasMany(Price::class);
+        return $this->belongsTo(Price::class);
     }
 }
