a37f06ff636446ad8256886c214dae85e1632efd
Obcoder <obcoder@gmail.com>
2025-05-31 16:16:46 +0300

    Verwalter: dev-20250531-03-Geography-Cities_fix-04

--- resources/js/Pages/Verwalter.vue
+++ resources/js/Pages/Verwalter.vue
@@ -688,7 +688,8 @@ useHead({
                                                 <v-col>
                                                     <v-sheet>
                                                         <div v-for="city in filteredCities"
-                                                             class="inline-block mr-2 p-1 text-xs text-teal-200">
+                                                             class="inline-block mr-2 p-1 text-xs text-teal-200 hover:bg-teal-200 hover:text-black"
+                                                        >
                                                             <div>
                                                                 <Link :href="route('city.show', city.id)">
                                                                     {{city.name}}
