b727e8ee0c1c3047b8da9ecdb0c5b305ffc2ff6c
Obcoder <obcoder@gmail.com>
2025-06-22 15:29:19 +0300

    Verwalter: dev-Products-20250622-05/Banner_row2

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -119,8 +119,12 @@ useHead({
                     </v-card-text>
                 </v-card>
             </v-col>
-            <v-col cols="3"></v-col>
-            <v-col cols="3"></v-col>
+            <v-col>
+                <v-img src="https://storage.yandexcloud.net/pps/banners/%D0%91%D0%B0%D0%BD%D0%BD%D0%B5%D1%80%20%D0%BB%D0%B5%D1%86%D0%B5%D1%82%D0%B8%D0%BD_%D0%9B%D0%B5%D1%86%D0%B5%D1%82%D0%B8%D0%BD%20%D0%BA%D0%BE%D0%BF%D0%B8%D1%8F.png"
+                       cover
+                       rounded
+                ></v-img>
+            </v-col>
             <v-col cols="3">
                 <v-card>
                     <v-img src="https://storage.yandexcloud.net/cold-reserve/GIElems/shutterstock_1476761888-3.webp"
