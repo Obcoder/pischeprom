2df4617e8426e47024f3225b97a6f5430780c6cf
Obcoder <obcoder@gmail.com>
2025-07-30 19:05:39 +0300

    Ameise/Check: dev-20250730-05/Measure_fix-02

--- app/Models/check_commodity.php
+++ app/Models/check_commodity.php
@@ -8,8 +8,9 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 class check_commodity extends Pivot
 {
     protected $table = 'check_commodity';
+
     public function measure(): BelongsTo
     {
-        return $this->belongsTo(measure::class, 'measure_id', 'id');
+        return $this->belongsTo(measure::class, 'measure_id');
     }
 }
