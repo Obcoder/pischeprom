e334953099574c2920abe80e512bcf7ceccbffd5
Obcoder <obcoder@gmail.com>
2025-07-30 17:43:27 +0300

    Ameise/Check: dev-20250730-03/mark-up_fix-01

--- app/Models/Check.php
+++ app/Models/Check.php
@@ -28,7 +28,7 @@ class Check extends Model
     {
         return $this->belongsToMany(Commodity::class)
             ->using(check_commodity::class)
-            ->withPivot('quantity', 'measure_id', 'price', 'total_price');
+            ->withPivot('quantity', 'measure', 'price', 'total_price');
     }
     public function measure()
     {
