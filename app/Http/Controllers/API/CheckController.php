0925f9c3d7bf3480f061eec7d18221dc60923b60
Obcoder <obcoder@gmail.com>
2025-07-30 18:23:54 +0300

    Ameise/Check: dev-20250730-03/mark-up_fix-12

--- app/Http/Controllers/API/CheckController.php
+++ app/Http/Controllers/API/CheckController.php
@@ -31,7 +31,7 @@ class CheckController extends Controller
      */
     public function show(string $id)
     {
-        $check = Check::with('commodities.measure')
+        $check = Check::with('commodities')
             ->findOrFail($id);
         return Inertia::render('Ameise/Check', ['check'=>$check]);
     }
