9d9f2ffb6f19a4329e48dfbf703d9633cb23b136
Obcoder <obcoder@gmail.com>
2025-08-14 19:10:32 +0300

    Ameise: dev-20250814-04/Entities from Sales (distinct)_fix-01

--- app/Http/Controllers/API/SaleController.php
+++ app/Http/Controllers/API/SaleController.php
@@ -15,7 +15,6 @@ class SaleController extends Controller
     public function index()
     {
         $sales = Sale::orderBy('date', 'desc')
-            ->with('entity.distinct')
             ->get();
         return $sales;
     }
