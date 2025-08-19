5f3be32d535f22d71d13f8872d8e789a88a2e07c
Obcoder <obcoder@gmail.com>
2025-08-19 11:24:17 +0300

    Ameise/Product: dev-20250819-03/Sales by Product table dev_fix-02

--- app/Http/Controllers/API/SaleController.php
+++ app/Http/Controllers/API/SaleController.php
@@ -21,7 +21,9 @@ class SaleController extends Controller
             $query->byProduct($productId); // <-- используем scope
         }
 
-        return $query->with('goods.products')->get();
+        return $query->with('goods.products')
+            ->withPivot('quantity')
+            ->get();
     }
 
 
