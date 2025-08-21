798dd2831fa6552c7c553b402183b4cb25a8b637
Obcoder <obcoder@gmail.com>
2025-08-21 12:03:44 +0300

    email_entity pivot created; field_unit pivot created

---
+++ database/migrations/2025_08_21_115926_create_email_entity_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('email_entity', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->foreignId('email_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('entity_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('email_entity');
+    }
+};
