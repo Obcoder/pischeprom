39cf93e3b53114ea2119bfa98b0187247eec13a2
Obcoder <obcoder@gmail.com>
2026-01-07 06:58:02 +0300

    web.php dev-2026-01-05-rev-05

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::middleware([
     'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
+        return Inertia::render('Grossbuch');
     })->name('dashboard');
 });
 
