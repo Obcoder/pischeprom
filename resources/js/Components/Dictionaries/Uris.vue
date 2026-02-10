3488ca99be4dff6f3d9d386cab37983c3ae08ea8
Obcoder <obcoder@gmail.com>
2026-02-10 22:39:46 +0300

    UriController: dev-20260210-02-fix-13

--- resources/js/Components/Dictionaries/Uris.vue
+++ resources/js/Components/Dictionaries/Uris.vue
@@ -29,6 +29,7 @@ const fetchUris = async () => {
         }
     })
 
+    alert(data)
     uris.value = data
     loading.value = false
 }
@@ -77,100 +78,42 @@ function storeUri(){
 
 </script>
 
-
-
 <template>
 
-<!--    <v-row>-->
-<!--        <v-col lg="2">-->
-<!--            <v-text-field v-model="searchUris"-->
-<!--                          label="Поиск uris"-->
-<!--                          variant="solo-filled"-->
-<!--                          color="rose"-->
-<!--                          density="comfortable"-->
-<!--                          hide-details-->
-<!--            ></v-text-field>-->
-<!--        </v-col>-->
-<!--    </v-row>-->
-<!--    <v-row>-->
-<!--        <v-col>-->
-<!--            <v-data-table :items="filteredUris"-->
-<!--                          items-per-page="1000"-->
-<!--                          :headers="headerUris"-->
-<!--                          fixed-header-->
-<!--                          height="888px"-->
-<!--                          density="compact"-->
-<!--                          class="text-xs border rounded border-red-50"-->
-<!--                          hover-->
-<!--            >-->
-<!--                <template v-slot:item.address="{item}">-->
-<!--                    <a :href="item.address" target="_blank"-->
-<!--                       class="text-xs text-green-400 inline-block"-->
-<!--                    >{{item.address}}</a>-->
-<!--                </template>-->
-<!--                <template v-slot:item.units="{item}">-->
-<!--                    <div v-for="unit in item.units">-->
-<!--                        <Link :href="route('web.unit.show', unit.id)">{{unit.name}}</Link>-->
-<!--                    </div>-->
-<!--                </template>-->
-<!--                <template v-slot:item.created_at="{item}">-->
-<!--                    <span class="text-xs font-sans">{{date.format(item.created_at, 'fullDate')}}</span>-->
-<!--                </template>-->
-<!--            </v-data-table>-->
-<!--        </v-col>-->
-<!--    </v-row>-->
+    <v-text-field
+        v-model="search"
+        label="Поиск uris"
+        variant="solo-filled"
+        color="rose"
+        density="comfortable"
+        hide-details
+        clearable
+    />
+    <v-data-table
+        :headers="headerUris"
+        :items="uris"
+        items-per-page="1000"
+        fixed-header
+        height="888px"
+        density="compact"
+        class="text-xs border rounded border-red-50"
+        hover
+    >
+        <template v-slot:item.address="{item}">
+            <a :href="item.address" target="_blank"
+               class="text-xs text-green-400 inline-block"
+            >{{item.address}}</a>
+        </template>
+        <template v-slot:item.units="{item}">
+            <div v-for="unit in item.units">
+                <Link :href="route('web.unit.show', unit.id)">{{unit.name}}</Link>
+            </div>
+        </template>
+        <template v-slot:item.created_at="{item}">
+            <span class="text-xs font-sans">{{date.format(item.created_at, 'fullDate')}}</span>
+        </template>
+    </v-data-table>
 
-    <v-row>
-        <v-col lg="2">
-            <v-text-field
-                v-model="search"
-                label="Поиск uris"
-                variant="solo-filled"
-                color="rose"
-                density="comfortable"
-                hide-details
-                clearable
-            />
-        </v-col>
-    </v-row>
-
-    <v-row>
-        <v-col>
-            <v-data-table
-                :items="uris"
-                items-per-page="1000"
-                fixed-header
-                height="888px"
-                density="compact"
-                class="text-xs border rounded border-red-50"
-                hover
-            >
-<!--                <template #item.address="{ item }">-->
-<!--                    <a-->
-<!--                        :href="item.address"-->
-<!--                        target="_blank"-->
-<!--                        class="text-xs text-green-400 inline-block"-->
-<!--                    >-->
-<!--                        {{ item.address }}-->
-<!--                    </a>-->
-<!--                </template>-->
-
-<!--                <template #item.units="{ item }">-->
-<!--                    <div v-for="unit in item.units" :key="unit.id">-->
-<!--                        <Link :href="route('web.unit.show', unit.id)">-->
-<!--                            {{ unit.name }}-->
-<!--                        </Link>-->
-<!--                    </div>-->
-<!--                </template>-->
-
-<!--                <template #item.created_at="{ item }">-->
-<!--                        <span class="text-xs font-sans">-->
-<!--                          {{ date.format(item.created_at, 'fullDate') }}-->
-<!--                        </span>-->
-<!--                    </template>-->
-            </v-data-table>
-        </v-col>
-    </v-row>
 </template>
 
 <style scoped>
