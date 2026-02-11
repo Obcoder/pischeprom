bef7a5bb5df0bd0572fe84d56dfb2f3cfa7289c7
Obcoder <obcoder@gmail.com>
2026-02-11 23:34:53 +0300

    Uris: dev-20260211-02

--- resources/js/Components/Dictionaries/Uris.vue
+++ resources/js/Components/Dictionaries/Uris.vue
@@ -14,10 +14,10 @@ const loading = ref(false)
 
 const headerUris = [
     { title: 'Address', key: 'address' },
-    // { title: 'Units', key: 'units' },
-    // { title: 'Valid', key: 'is_valid' },
-    // { title: 'Follow', key: 'follow' },
-    // { title: 'Created', key: 'created_at' },
+    { title: 'Units', key: 'units' },
+    { title: 'Valid', key: 'is_valid' },
+    { title: 'Follow', key: 'follow' },
+    { title: 'Created', key: 'created_at' },
 ]
 
 const fetchUris = async () => {
@@ -29,15 +29,12 @@ const fetchUris = async () => {
         }
     })
 
-    alert(data)
     uris.value = data
     loading.value = false
 }
 
 // 🔹 загрузка при старте
-onMounted(()=>{
-    fetchUris()
-})
+fetchUris()
 
 // 🔹 простой debounce
 // let timeout = null
@@ -45,6 +42,7 @@ onMounted(()=>{
 //     clearTimeout(timeout)
 //     timeout = setTimeout(fetchUris, 400)
 // })
+
 
 const filteredUris = computed(()=>{
     if (!search.value) return uris.value
@@ -99,19 +97,19 @@ function storeUri(){
         class="text-xs border rounded border-red-50"
         hover
     >
-        <template v-slot:item.address="{item}">
-            <a :href="item.address" target="_blank"
-               class="text-xs text-green-400 inline-block"
-            >{{item.address}}</a>
-        </template>
-        <template v-slot:item.units="{item}">
-            <div v-for="unit in item.units">
-                <Link :href="route('web.unit.show', unit.id)">{{unit.name}}</Link>
-            </div>
-        </template>
-        <template v-slot:item.created_at="{item}">
-            <span class="text-xs font-sans">{{date.format(item.created_at, 'fullDate')}}</span>
-        </template>
+<!--        <template v-slot:item.address="{item}">-->
+<!--            <a :href="item.address" target="_blank"-->
+<!--               class="text-xs text-green-400 inline-block"-->
+<!--            >{{item.address}}</a>-->
+<!--        </template>-->
+<!--        <template v-slot:item.units="{item}">-->
+<!--            <div v-for="unit in item.units">-->
+<!--                <Link :href="route('web.unit.show', unit.id)">{{unit.name}}</Link>-->
+<!--            </div>-->
+<!--        </template>-->
+<!--        <template v-slot:item.created_at="{item}">-->
+<!--            <span class="text-xs font-sans">{{date.format(item.created_at, 'fullDate')}}</span>-->
+<!--        </template>-->
     </v-data-table>
 
 </template>
