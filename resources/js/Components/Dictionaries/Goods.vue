b9a879d4a34c9e5bb1a8047e946359b887b7bff9
Obcoder <obcoder@gmail.com>
2026-03-09 20:46:33 +0300

    Goods.vue: dev-20260309-01#vat_rate to Goods-cor-01

--- resources/js/Components/Dictionaries/Goods.vue
+++ resources/js/Components/Dictionaries/Goods.vue
@@ -51,7 +51,7 @@ const headers = [
     { key: 'group_category', title: 'Category', sortable: false, width: '175px' },
     { key: 'ava_image', title: '', sortable: false, width: '200px' },
     { key: 'name', title: 'Good', sortable: true },
-    { key: 'vat_rate', title: 'VAT', sortable: false, width: '140px' },
+    { key: 'vatRate', title: 'VAT', sortable: false, width: '140px' },
     { key: 'is_published', title: 'Pub', sortable: true, width: '90px' },
     { key: 'created_at', title: 'Создан', sortable: true, width: '200px' },
     { key: 'actions', title: '', sortable: false, width: '120px' },
@@ -442,7 +442,7 @@ onBeforeUnmount(() => {
                         </Link>
                     </template>
 
-                    <template #item.vat_rate="{ item }">
+                    <template #item.vatRate="{ item }">
                         <span v-if="item.vatRate">
                             {{ item.vatRate.title }} ({{ item.vatRate.rate }}%)
                         </span>
