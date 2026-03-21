0cadc7699192ef04ef11bc03c793caa1ba062aa8
Obcoder <obcoder@gmail.com>
2026-03-21 21:33:42 +0300

    Categories: dev-20260321-01-fix-08

--- resources/js/Pages/Categories.vue
+++ resources/js/Pages/Categories.vue
@@ -13,7 +13,7 @@ const props = defineProps({
 const category = ref()
 
 function fetchCategory(id){
-    axios.get(route('category.show', id)).then(function (response){
+    axios.get(route('category.show', category)).then(function (response){
         category.value = response.data
     }).catch(function (error){
         console.log(error)
