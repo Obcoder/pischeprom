7f4a5501afa5c00309d3c771bed40b0304b43456
Obcoder <obcoder@gmail.com>
2026-03-21 21:21:27 +0300

    Categories: dev-20260321-01-fix-04

--- app/Http/Controllers/Web/CategoryController.php
+++ app/Http/Controllers/Web/CategoryController.php
@@ -63,7 +63,7 @@ class CategoryController extends Controller
     {
         $category->load('products')->loadCount('products');
 
-        return Inertia::render('Categories', [
+        return Inertia::render('/Pages/Categories', [
             'category' => new CategoryResource($category),
         ]);
     }
